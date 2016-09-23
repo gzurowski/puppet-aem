7ca96168c0615b8bc54b65836f285d0dd0761327
Gregor Zurowski <gregor@zurowski.net>
2016-09-23 12:00:24 +0200

    Revert "Move creation of start scripts into package class"
    This reverts commit 2729d3db396752918fe9cc4c47ea85cc431e994f.

--- manifests/config.pp
+++ manifests/config.pp
@@ -18,6 +18,36 @@ define aem::config(
   $type,
   $user
 ) {
+
+  File {
+    group => $group,
+    owner => $user,
+  }
+
+
+  # Create the env script
+  file { "${home}/crx-quickstart/bin/start-env":
+    ensure  => file,
+    content => template("${module_name}/start-env.erb"),
+    mode    => '0775',
+  }
+
+  # Rename the original start script.
+  file { "${home}/crx-quickstart/bin/start.orig":
+    ensure  => file,
+    replace => false,
+    source  => "${home}/crx-quickstart/bin/start",
+    mode    => '0775',
+  }
+
+  # Create the start script
+  file { "${home}/crx-quickstart/bin/start":
+    ensure  => file,
+    content => template("${module_name}/start.erb"),
+    mode    => '0775',
+    require => File["${home}/crx-quickstart/bin/start.orig"],
+  }
+
   if $osgi_configs {
 
     if is_array($osgi_configs) {
